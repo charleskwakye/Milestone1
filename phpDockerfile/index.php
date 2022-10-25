93e07dc191d0925f2018fb59169056e7c455113f
charleskwakye <r0879035@student.thomasmore.be>
2022-10-25 23:43:45 +0000

    third

---
+++ phpDockerfile/index.php
@@ -0,0 +1,13 @@
+h1>Hello Charles</h1>
+<?php
+$host = 'mysql-container';
+$user = 'root';
+$pass = '2000';
+$db = 'milstone1db';
+$conn = new mysqli($host, $user, $pass,$db);
+
+if($conn->connect_error) {
+    echo 'Connection failed: ' . $conn->connect_error;
+}
+echo "Connected to MySQL successfully!";
+?>
