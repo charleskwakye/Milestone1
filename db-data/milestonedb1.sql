e87934ce94b07594f1a7da9b00a53a8b56f7a0f1
charleskwakye <r0879035@student.thomasmore.be>
2022-10-26 13:25:27 +0000

    fix

---
+++ db-data/milestonedb1.sql
@@ -0,0 +1,2 @@
+create table users(name TEXT);
+insert into users(name) values("Charles Nana Kwakye");
--- phpDockerfile/index.php
+++ phpDockerfile/index.php
@@ -1,9 +1,9 @@
-h1>Hello Charles</h1>
+<h1>Hello Charles</h1>
 <?php
 $host = 'mysql-container';
 $user = 'root';
 $pass = '2000';
-$db = 'milstone1db';
+$db = 'milestone1db';
 $conn = new mysqli($host, $user, $pass,$db);
 
 if($conn->connect_error) {
